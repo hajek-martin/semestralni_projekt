3e8f4c796c095921b11822e1af8bf632bd97ca05
martin.hajek1 <martin.hajek1@tul.cz>
2021-12-01 15:35:56 +0100

    x

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const express = require("express");
-
-//logování
-var morgan = require('morgan');
-// const bodyParser = require("body-parser"); /* deprecated */
+const express = require('express');
+const morgan = require('morgan');
 const cors = require("cors");
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -13,18 +11,17 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
-// parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
-
-// Práce s userem
+// Routes
 require("./app/routes/user.routes.js")(app);
 require("./app/routes/login.routes")(app)
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`http://localhost/${PORT}.`);
 });
