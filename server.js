3868de75c069ba20e926c110cb5e579676fe3560
martin.hajek1 <martin.hajek1@tul.cz>
2021-12-01 01:25:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+//logování
+var morgan = require('morgan');
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
+/*
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});*/
+
+// Práce s userem
+require("./app/routes/user.routes.js")(app);
+require("./app/routes/login.routes")(app)
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
